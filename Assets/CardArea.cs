054499fa598d26efff590555cada2f24e84fc6a0
Wang Gaunyu <smartwgy@gmail.com>
2019-12-07 11:31:29 +0900

    Base Update

---
+++ Assets/CardArea.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class CardArea : MonoBehaviour,IPointerEnterHandler
+{
+    Card QccupiedCard;
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+       
+        if (CardManager.Instance.CurrentSelectedCard != null)
+        {
+            Debug.Log("Area Enter");
+            CardManager.Instance.CurrentSelectedCard.gameObject.transform.position = gameObject.transform.position;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
